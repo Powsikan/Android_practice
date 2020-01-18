95cb2e1e069dfe98fdf4e1a5fe2452136ec47163
powsokan <powsi1997@gmail.com>
2020-01-18 18:30:53 +0530

    Layout designs

---
+++ app/src/main/java/com/powsikan/kuppi/MainActivity.java
@@ -0,0 +1,66 @@
+package com.powsikan.kuppi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MyAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private boolean mchecked;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button =(Button)findViewById(R.id.button);
+        final EditText editText=(EditText)findViewById(R.id.editText) ;
+       final CheckBox checkBox=(CheckBox)findViewById(R.id.checkbox);
+
+       checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+             mchecked=isChecked;
+           }
+       });
+
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(new ArrayList<PersonInfo>(),mchecked);
+        recyclerView.setAdapter(mAdapter);
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String s = editText.getText().toString();
+               
+
+            }
+        });
+    }
+}
